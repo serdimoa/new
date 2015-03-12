2993ebdb836d4ad22ec784acb65ff613668e1c59
serdimoa <serdimoa@gmail.com>
2015-03-12 15:39:50 +0500

    dev serv up

--- config.php
+++ config.php
@@ -7,7 +7,8 @@
  */
 require_once("connector/data_connector.php");
 $dbtype = "MySQL";
-$conn = mysql_connect("localhost","root","27051993");
+//$conn = mysql_connect("localhost","root","27051993");//dev
+$conn = mysql_connect("localhost", "root", "FTuRhnYc13q4");//serv
 ConnectorSecurity::$security_key = true;
 ConnectorSecurity::$xss = DHX_SECURITY_SAFEHTML;
 ConnectorSecurity::$xss = DHX_SECURITY_TRUSTED;
